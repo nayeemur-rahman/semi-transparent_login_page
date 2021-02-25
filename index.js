28aa1a4cb95dbd09f63241286e81e55b62801406
nayeemur-rahman <68645079+nayeemur-rahman@users.noreply.github.com>
2021-02-25 21:08:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const loginHeadline = document.querySelector("#login-headline");
+const usernameInputBox = document.querySelector("#username");
+const passwordInputBox = document.querySelector("#password");
+const submitBtn = document.querySelector("#login-btn");
+const ltpTogglerBtn = document.querySelector("#ltp-toggler");
+loginHeadline.addEventListener("click", ()=>{
+    usernameInputBox.focus();
+});
+
+usernameInputBox.addEventListener("keypress", (event) => {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    if (usernameInputBox.value == ''){
+        return;
+    }
+    passwordInputBox.focus();
+  }
+});
+passwordInputBox.addEventListener("keypress", (event) => {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    if (usernameInputBox.value == '' || passwordInputBox.value.length < 6){
+        return;
+    }
+    else{
+        submitBtn.click();
+    }
+  }
+});
+ltpTogglerBtn.addEventListener("click", ()=>{
+  const container = document.querySelector("#container").classList;
+  if (container.contains("landscape")){
+    container.remove("landscape");
+  }
+  else{
+    container.add("landscape");
+  }
+})
